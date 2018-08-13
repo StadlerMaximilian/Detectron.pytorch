e53ac9696c978d130920fc08c1c0c7dfb3c53c06
ga63zoc <maximilian.stadler@tum.de>
2018-08-13 14:10:02 +0200

    bugfix

--- tools/train_net_step.py
+++ tools/train_net_step.py
@@ -145,7 +145,6 @@ def cycle(iterable):
 
 def main():
     """Main function"""
-    multiprocessing.set_start_method('spawn', force=True)
     args = parse_args()
     print('Called with args:')
     print(args)
