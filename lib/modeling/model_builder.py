74c7c2d0aac9cb9813ca12e3231a375f84699474
ga63zoc <maximilian.stadler@tum.de>
2018-08-23 17:35:22 +0200

    bugfix

--- lib/modeling/model_builder.py
+++ lib/modeling/model_builder.py
@@ -165,9 +165,9 @@ class Generalized_RCNN(nn.Module):
         rpn_ret = self.RPN(blob_conv, im_info, roidb)
 
         #debug
-        for k, v in rpn_ret:
-            print(rpn_ret)
-            #print("{}: {}\n".format(k, v))
+        #for k, v in rpn_ret:
+        print(rpn_ret)
+        #print("{}: {}\n".format(k, v))
 
         # if self.training:
         #     # can be used to infer fg/bg ratio
