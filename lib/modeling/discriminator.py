fa141411017a749e9fd01187668f5c0be4a1f8b5
ga63zoc <maximilian.stadler@tum.de>
2018-08-23 17:46:01 +0200

    bugfix

--- lib/modeling/discriminator.py
+++ lib/modeling/discriminator.py
@@ -39,6 +39,10 @@ class Discriminator(nn.Module):
     def forward(self, blob_conv, rpn_ret, adv_target=-1.0):
         return_dict = {}
 
+        #debug
+        for k, v in rpn_ret.items():
+            print("{}: {}".format(k, type(v)))
+
         batch_size = len(rpn_ret['labels_int32'])
         #print("batch_size in discriminator: {}".format(batch_size))
         adv_score = self.adversarial(blob_conv.view(batch_size, -1))
