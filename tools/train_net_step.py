e17d55f7bd2ce9e6c02ed50a2ab13d61a6c3d189
ga63zoc <maximilian.stadler@tum.de>
2018-09-02 14:43:20 +0200

    bugfix

--- tools/train_net_step.py
+++ tools/train_net_step.py
@@ -466,7 +466,7 @@ def main():
             args_test = Namespace(cfg_file='{}'.format(args.cfg_file), dataset=None,
                                   load_ckpt='{}'.format(final_model), load_detectron=None,
                                   multi_gpu_testing=True, output_dir='{}'.format(test_output_dir),
-                                  range=None, set_cfgs=set_cfgs, vis=False)
+                                  range=None, set_cfgs=args.set_cfgs, vis=False)
         else:
             args_test = Namespace(cfg_file='{}'.format(args.cfg_file), dataset=None,
                                   load_ckpt='{}'.format(final_model), load_detectron=None,
