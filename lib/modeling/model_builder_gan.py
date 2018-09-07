7554ad2413f20d687452cbb7b49c3bb45ba6a50c
ga63zoc <maximilian.stadler@tum.de>
2018-09-07 15:29:14 +0200

    bugfix

--- lib/modeling/model_builder_gan.py
+++ lib/modeling/model_builder_gan.py
@@ -99,3 +99,8 @@ class GAN(nn.Module):
             self.orphans_in_detectron = d_orphan
 
         return self.mapping_to_detectron, self.orphans_in_detectron
+
+    def train(self, mode=True):
+        super().train(mode)
+        self.generator.train(mode)
+        self.discriminator.trai(mode)
