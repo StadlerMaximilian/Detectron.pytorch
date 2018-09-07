783b4d96e447a8bdbc157771a149f70139ed187a
ga63zoc <maximilian.stadler@tum.de>
2018-09-07 16:12:36 +0200

    bugfix

--- lib/utils/net.py
+++ lib/utils/net.py
@@ -75,13 +75,11 @@ def update_learning_rate(optimizer, cur_lr, new_lr, type='default'):
         # Update learning rate, note that different parameter may have different learning rate
         param_keys = []
         for ind, param_group in enumerate(optimizer.param_groups):
-            print(param_group.keys())
             if ind == 1 and cfg.SOLVER.BIAS_DOUBLE_LR:  # bias params
                 param_group['lr'] = new_lr * 2
             else:
                 param_group['lr'] = new_lr
             param_keys += param_group['params']
-            print(param_group['params'])
 
         if cfg.GAN.GAN_MODE_ON:
             if type == 'pre':
@@ -111,7 +109,6 @@ def _CorrectMomentum(optimizer, param_keys, correction):
     compatible in scale with lr * grad.
     """
     logger.info('Scaling update history by %.6f (new lr / old lr)', correction)
-    print(optimizer.state.keys())
 
     for p_key in param_keys:
         optimizer.state[p_key]['momentum_buffer'] *= correction
