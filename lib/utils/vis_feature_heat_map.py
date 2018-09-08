3455232f4888e4186289d6d623cfca763e32ea2f
ga63zoc <maximilian.stadler@tum.de>
2018-09-08 23:30:56 +0200

    bugfix

--- lib/utils/vis_feature_heat_map.py
+++ lib/utils/vis_feature_heat_map.py
@@ -34,33 +34,33 @@ def show_heat_maps(blob_real, blob_fake, blob_residual, output_dir, im_name, blo
             plt.title('RoI from Image')
 
             plt.subplot(1, 4, 4*batch + 2)
-            plt.imshow(create_heat_map(blob_real[batch, :, :, :]))
+            plt.imshow(create_heat_map(blob_real[batch, :, :, :]), cmap='heat')
             plt.show()
             plt.title('Real RoI')
 
             plt.subplot(1, 4, 4*batch + 3)
-            plt.imshow(create_heat_map(blob_fake[batch, :, :, :]))
+            plt.imshow(create_heat_map(blob_fake[batch, :, :, :]), cmap='heat')
             plt.show()
             plt.title('Fake RoI')
 
             plt.subplot(1, 4, 4*batch + 4)
-            plt.imshow(create_heat_map(blob_residual[batch, :, :, :]))
+            plt.imshow(create_heat_map(blob_residual[batch, :, :, :]), cmap='heat')
             plt.show()
             plt.title('Residual RoI')
 
         else:
             plt.subplot(1, 3, 3 * batch + 1)
-            plt.imshow(create_heat_map(blob_real[batch, :, :, :]))
+            plt.imshow(create_heat_map(blob_real[batch, :, :, :]), cmap='heat')
             plt.show()
             plt.title('Real RoI')
 
             plt.subplot(1, 3, 3 * batch + 2)
-            plt.imshow(create_heat_map(blob_fake[batch, :, :, :]))
+            plt.imshow(create_heat_map(blob_fake[batch, :, :, :]), cmap='heat')
             plt.show()
             plt.title('Fake RoI')
 
             plt.subplot(1, 3, 3 * batch + 3)
-            plt.imshow(create_heat_map(blob_residual[batch, :, :, :]))
+            plt.imshow(create_heat_map(blob_residual[batch, :, :, :]), cmap='heat')
             plt.show()
             plt.title('Residual RoI')
 
