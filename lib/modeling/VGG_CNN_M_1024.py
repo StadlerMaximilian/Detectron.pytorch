d39f3b4bc1eee83af7bc142fc9f223f83d6c091f
ga63zoc <maximilian.stadler@tum.de>
2018-09-18 20:06:15 +0200

    bugfix

--- lib/modeling/VGG_CNN_M_1024.py
+++ lib/modeling/VGG_CNN_M_1024.py
@@ -105,10 +105,10 @@ class VGG_CNN_M_1024_fc_head(nn.Module):
         else:
             if cfg.DEBUG:
                 print("\tInit VGG-CNN-M-1024 FC-HEAD with XAVIER")
-            mynn.init.XavierFill(self.fc[0].weight)
-            init.constant(self.fc[0].bias, 0)
-            mynn.init.XavierFill(self.fc[2].weight)
-            init.constant(self.fc[2].bias, 0)
+            mynn.init.XavierFill(self.fc1[0].weight)
+            init.constant(self.fc1[0].bias, 0)
+            mynn.init.XavierFill(self.fc2[0].weight)
+            init.constant(self.fc2[0].bias, 0)
 
     def detectron_weight_mapping(self):
         detectron_weight_mapping = {
