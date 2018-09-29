a9625164ddc4b6e93e8ec94e683a5b282f1a6ddd
ga63zoc <maximilian.stadler@tum.de>
2018-09-29 22:56:57 +0200

    debugging

--- tools/gan_vis_features.py
+++ tools/gan_vis_features.py
@@ -156,7 +156,8 @@ def vis_features():
         box_proposals = None
         if args.proposal_file is not None:
             box_proposals = entry['boxes'][entry['gt_classes'] == 0]
-            print(box_proposals)
+            for box in box_proposals.shape[0]:
+                print(box_proposals[box])
 
         inputs, im_scale = _get_blobs(im, box_proposals, cfg.TEST.SCALE, cfg.TEST.MAX_SIZE)
 
